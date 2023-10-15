24acb538f84ddb80b707c135efd52399f8efe21c
Alex Ryndin <me@alexryndin.me>
2023-10-15 20:40:41 +0300

    path_descend function

--- dbw_extension.c
+++ dbw_extension.c
@@ -1,5 +1,6 @@
 /* Add your header comment here */
 #include <assert.h>
+#include <ctype.h>
 #include <dbg.h>
 #include <md4c-html.h>
 #include <sds.h>
@@ -21,6 +22,132 @@ __declspec(dllexport)
       "Couldn't append to string");
 error:
   return;
+}
+
+static int check_name(char *name) {
+  for (int i = 0; name[i] != '\0'; i++) {
+    if (!isalnum(name[i]) && name[i] != '_' && name[i] != '-') {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+static void
+sqlite3_path_descend(sqlite3_context *ctx, int argc, sqlite3_value **argv) {
+  if (argc != 5 || sqlite3_value_type(argv[0]) != SQLITE_TEXT ||
+      sqlite3_value_type(argv[1]) != SQLITE_TEXT ||
+      sqlite3_value_type(argv[2]) != SQLITE_TEXT ||
+      sqlite3_value_type(argv[3]) != SQLITE_TEXT ||
+      sqlite3_value_type(argv[4]) != SQLITE_TEXT) {
+    sqlite3_result_error(ctx, "Invalid arguments", -1);
+    return;
+  }
+
+  char **s_path = NULL;
+  char *q = NULL;
+  int ret = 1;
+  sqlite3 *db = sqlite3_context_db_handle(ctx);
+  sqlite3_stmt *stmt = NULL;
+
+  char *path = (char *)sqlite3_value_text(argv[0]);
+  if (!strcmp(path, "") || !strcmp(path, "/")) {
+    sqlite3_result_int64(ctx, ret);
+    goto exit;
+  }
+  char *table_name = (char *)sqlite3_value_text(argv[1]);
+  if (!strcmp(table_name, "")) {
+    table_name = "snippets";
+  }
+  char *column_name = (char *)sqlite3_value_text(argv[2]);
+  if (!strcmp(column_name, "")) {
+    column_name = "dirs";
+  }
+  char *id_column_name = (char *)sqlite3_value_text(argv[3]);
+  if (!strcmp(id_column_name, "")) {
+    id_column_name = "id";
+  }
+  char *parent_id_column_name = (char *)sqlite3_value_text(argv[4]);
+  if (!strcmp(parent_id_column_name, "")) {
+    parent_id_column_name = "parent_id";
+  }
+
+  if (!(check_name(table_name) && check_name(column_name) &&
+        check_name(id_column_name) && check_name(parent_id_column_name))) {
+    sqlite3_result_error(ctx, "Invalid arguments", -1);
+  }
+
+  q = sdsnew("");
+  if (q == NULL) {
+    sqlite3_result_error_nomem(ctx);
+    goto exit;
+  }
+  q = sdscatprintf(
+      q,
+      "select a.%s from %s as a"
+      " join %s as b on b.%s == a.%s"
+      " where b.%s = ? and a.name = ?",
+      id_column_name,
+      table_name,
+      table_name,
+      id_column_name,
+      parent_id_column_name,
+      id_column_name);
+  if (q == NULL) {
+    sqlite3_result_error_nomem(ctx);
+    goto exit;
+  }
+  int cnt;
+  s_path = sdssplitlen(path, strlen(path), "/", 1, &cnt);
+  if (s_path == NULL) {
+    sqlite3_result_error_nomem(ctx);
+    goto exit;
+  }
+  for (int i = 1; i < cnt; i++) {
+    if (sdslen(s_path[i]) == 0) {
+      continue;
+    }
+    CHECK(
+        sqlite3_prepare_v2(db, q, sdslen(q) + 1, &stmt, NULL) == SQLITE_OK,
+        "Couldn't prepare statement: %s",
+        sqlite3_errmsg(db));
+    CHECK(
+        sqlite3_bind_int64(stmt, 1, ret) == SQLITE_OK,
+        "Couldn't bind parameter to statement");
+    CHECK(
+        sqlite3_bind_text(stmt, 2, s_path[i], sdslen(s_path[i]), NULL) ==
+            SQLITE_OK,
+        "Couldn't bind parameter to statement");
+    int err = sqlite3_step(stmt);
+    switch (err) {
+    case SQLITE_DONE:
+      ret = -1;
+      goto exit;
+      break;
+    case SQLITE_ROW:
+      ret = sqlite3_column_int64(stmt, 0);
+      break;
+    default:
+      LOG_ERR("Couldn't get row from table: %s", sqlite3_errmsg(db));
+      goto error;
+    }
+  }
+
+exit:
+  if (q != NULL) {
+    sdsfree(q);
+  }
+  if (s_path != NULL) {
+    sdsfreesplitres(s_path, cnt);
+  }
+  if (stmt != NULL) {
+    sqlite3_finalize(stmt);
+  }
+  sqlite3_result_int(ctx, ret);
+  return;
+error:
+  sqlite3_result_error(ctx, "db error", -1);
+  goto exit;
 }
 
 static void
@@ -125,6 +252,17 @@ int sqlite3_dbwextension_init(
 
   sqlite3_create_function_v2(
       db, "md2html", 1, SQLITE_UTF8, NULL, sqlite3_md2html, NULL, NULL, NULL);
+
+  sqlite3_create_function_v2(
+      db,
+      "path_descend",
+      5,
+      SQLITE_UTF8,
+      NULL,
+      sqlite3_path_descend,
+      NULL,
+      NULL,
+      NULL);
 
 error:
   return rc;
